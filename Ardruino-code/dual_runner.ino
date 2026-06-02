bc37072ccdb552ef33132169cafc23f828f8e614
Hemansh Singh <morrisbuilds123@gmail.com>
2026-06-02 10:34:13 +0530

    Create dual_runner.ino
    Two lights meet in the center.

---
+++ Ardruino-code/dual_runner.ino
@@ -0,0 +1,19 @@
+int leds[] = {2,3,4,5,6,7,8,9,10,11,12,13};
+
+void setup() {
+  for(int i=0;i<12;i++) pinMode(leds[i], OUTPUT);
+}
+
+void loop() {
+
+  for(int i=0;i<6;i++) {
+
+    digitalWrite(leds[i], HIGH);
+    digitalWrite(leds[11-i], HIGH);
+
+    delay(120);
+
+    digitalWrite(leds[i], LOW);
+    digitalWrite(leds[11-i], LOW);
+  }
+}
